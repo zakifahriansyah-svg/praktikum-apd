bfa93cb39a1e7ec4305bca569b2ffa1caa22afe9
zakifahriansyah-svg <zaki.fahriansyah@gmail.com>
2025-09-25 13:56:04 +0800

    pertemuan 3

---
+++ kelas/Pertemuan 3/pertemuan3.py
@@ -0,0 +1,39 @@
+#nilai = 70
+
+#if nilai > 75:
+    #print('Nilai A')
+#elif nilai > 65:
+    #print("Nilai B")
+#else:
+    #print('Nilai C')
+
+
+# suhu = 35
+# if suhu > 35:
+#     print('panas')
+# if suhu > 30:
+#     print('sauna')
+# elif suhu > 20:
+#     print('normal')
+# else:
+#     print('sejuk')
+
+
+# umur = int(input('Masukkan umur anda: '))
+# if umur < 13:
+#     print('anak')
+
+
+# tinggi = int(input('masukkan umur anda:'))
+# if tinggi >= 145:
+#     print('boleh menaiki wahana')
+# else:
+#     print('tidak boleh menaiki wahana')
+
+belanja = float(input('Masukkan total belanja anda: '))
+if belanja > 100.000:
+    print('mendapatkan siskon 20%')
+elif belanja > 50.000:
+    print('mendapatkan diskon 10%')
+else:
+    print('tidak mendapat diskon')
