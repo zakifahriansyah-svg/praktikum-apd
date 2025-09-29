9b4955ec2e6f4b2a690bb13702a84966bce4ab56
zakifahriansyah-svg <zaki.fahriansyah@gmail.com>
2025-09-29 20:38:21 +0800

    Upload Posttest 3

--- kelas/Pertemuan 3/pertemuan3.py
+++ kelas/Pertemuan 3/pertemuan3.py
@@ -7,7 +7,6 @@
 #else:
     #print('Nilai C')
 
-
 # suhu = 35
 # if suhu > 35:
 #     print('panas')
@@ -22,6 +21,36 @@
 # umur = int(input('Masukkan umur anda: '))
 # if umur < 13:
 #     print('anak')
+# misalnya, umur = 16
+# Percabangan
+# if umur < 13:
+#     kategori = "Anak-anak"
+# elif umur < 18:
+#     kategori = "Remaja"
+# elif umur < 60:
+#     kategori = "Dewasa"
+# else:
+#     kategori = "Lansia"
+# # Menampilkan umur dan kategori
+# print("Umur:", umur, "Kategori:", kategori)
+
+# variabel = nilai_jika_true if kondisi else nilai_jika_false
+
+# nilai = 70
+# if nilai = 60:
+#     status = "Lulus"
+# else:
+#     status = "Tidak Lulus"
+# print(status)
+# # output
+# Lulus
+
+# # Menggunakan Ternary Operator
+# nilai = 70
+# status = "Lulus" if nilai = 60 else "Tidak Lulus"
+# print(status)
+# # output
+# Lulus
 
 
 # tinggi = int(input('masukkan umur anda:'))
@@ -30,10 +59,17 @@
 # else:
 #     print('tidak boleh menaiki wahana')
 
-belanja = float(input('Masukkan total belanja anda: '))
-if belanja > 100.000:
-    print('mendapatkan siskon 20%')
-elif belanja > 50.000:
-    print('mendapatkan diskon 10%')
-else:
-    print('tidak mendapat diskon')
+# belanja = float(input('Masukkan total belanja anda: '))
+# if belanja > 100000:
+#     diskon = belanja * 0.2
+#     print('mendapatkan siskon 20%')
+# elif belanja > 50000:
+#     diskon = belanja * 0.1
+#     print('mendapatkan diskon 10%')
+# else:
+#     print('tidak mendapat diskon')
+#     diskon = 0
+
+# total_bayar = belanja - diskon
+
+# print(f'Total bayar: {total_bayar}')
