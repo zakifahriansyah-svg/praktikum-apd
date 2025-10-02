cd9f210829577983dcb3ffb1e338dae882424dca
zakifahriansyah-svg <zaki.fahriansyah@gmail.com>
2025-10-02 14:16:08 +0800

    Pertemuan 3

---
+++ kelas/Pertemuan 4/pertemuan4.py
@@ -0,0 +1,40 @@
+# ulangi = 10
+# for i in range(ulangi):
+#     print(f'perulangan ke -{i}')
+
+# simpan = [1, 'Dapupu', 4.00, True]
+# for i in simpan:
+#     print(i)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for i in range(1, 4):# Mengontrol baris dalam tabel perkalian
+#     for j in range(1, 8):# Mengontrol kolom dalam tabel perkalian
+#         print(f'{i} x {j} = {i * j}')
+#     print('') #biar ada jarak tiap iterasi
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi? ")
+#     print(f"Total perulangan: {hitung}")
+
+# for i in range(1, 10):
+#     print('#' * i)
+
+# angka = [2, 5, 8, 12, 15, 7, 20]
+# print("Mencari angka pertama yang lebih besar dari 10...")
+# for i in angka:
+#     print(f"Sekarang memeriksa angka: {i}")
+#     if i > 10:
+#         print(f"Angka {i} lebih besar dari 10, Perulangan berhenti.")
+#         break
+# print("Program selesai.")
+
+for i in range(1, 11):
+    if i % 2 != 0:
+        continue
+    print(f"Angka genap ditemukan: {i}")
+print("\nProgram selesai.")
